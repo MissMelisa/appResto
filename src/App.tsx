5ea0c6e6f6f11a2d907785c921e0ec56f3147347
Rios Melisa <riosmelisa@Rioss-MacBook-Air.local>
2021-08-18 15:31:41 -0300

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import * as React from "react";
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react";
+import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import CartProvider from "./components/Context";
 import Home from "./pages/Home";
+import CartProvider from "./components/Context";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3} width="100%">
         <ColorModeSwitcher justifySelf="flex-end" />
-
         <Router>
           <CartProvider>
             <Switch>
